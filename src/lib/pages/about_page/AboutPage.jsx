d6c2799afe82e60c2844314c200e60a386da21b7
Nikhilesh Debbarma <nikhilesh.debbarma062@gmail.com>
2022-05-13 23:32:11 +0530

    master

---
+++ src/lib/pages/about_page/AboutPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AboutPage() {
+  return (
+    <div className="bg-green-700 text-[75px]">AboutPage</div>
+  )
+}
+
+export default AboutPage
---
+++ src/lib/pages/home_page/HomePage.jsx
@@ -0,0 +1,15 @@
+import React, {useContext} from 'react'
+
+
+function HomePage() {
+    // const {value, setValue} = useContext(AuthContext);
+
+  return (
+    <div className="">
+        <h2 className="bg-red-700 text-[75px]">{value}</h2>
+
+    </div>
+  )
+}
+
+export default HomePage
